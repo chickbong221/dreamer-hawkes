4e72f2b5116b2d3dffe8af1654e0088c17d2bf94
LTuan <letuanhf@gmail.com>
2026-05-19 17:51:52 +0900

    m

--- sweep_rgb.sh
+++ sweep_rgb.sh
@@ -4,43 +4,43 @@ set -euo pipefail
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_PushCube-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-PushCube-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_PickCube-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-PickCube-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_StackCube-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-StackCube-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_PegInsertionSide-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-PegInsertionSide-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_PushT-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-PushT-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_AnymalC-Reach-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-AnymalC-Reach-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
   --configs maniskill_rgb size50m \
   --task maniskill_UnitreeG1TransportBox-v1 \
-  --batch_size 16 \
+  --batch_size 8 \
   --logger.wandb_name dreamerv3-UnitreeG1TransportBox-v1-rgb-42-walltime_efficient
 
 python -m dreamerv3.main \
